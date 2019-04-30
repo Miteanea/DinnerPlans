00c13798fcee340c3840534d673b574f02f51dd7
dmitri.caragheaur <dmitri.caragheaur@gmail.com>
2019-04-30 16:48:35 +0200

    Recipe ID implemented with  a random number

--- DinnerPlans/Models/RecipeID.cs
+++ DinnerPlans/Models/RecipeID.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace DinnerPlans.Models
 {
-    class RecipeID
+    internal class RecipeID
     {
+        public RecipeID()
+        {
+            Random rnd = new Random();
+            ID = rnd.Next(1000);
+        }
+
+        private int ID { get; set; }
+
+        public override string ToString()
+        {
+            return ID.ToString();
+        }
     }
 }
