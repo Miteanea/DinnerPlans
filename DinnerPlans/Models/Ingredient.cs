8cdd0db2d42fe7d2186004e04001537b12d6b04c
dmitri.caragheaur <dmitri.caragheaur@gmail.com>
2019-04-30 16:47:07 +0200

    NutritionData and Ingredients classes added

---
+++ DinnerPlans/Models/Ingredient.cs
@@ -0,0 +1,11 @@
+﻿namespace DinnerPlans.Models
+{
+    public class Ingredient
+    {
+        // ID
+
+        // Name
+
+        public NutritionData NutritionData { get; }
+    }
+}
---
+++ DinnerPlans/Models/NutritionData.cs
@@ -0,0 +1,14 @@
+﻿namespace DinnerPlans.Models
+{
+    public class NutritionData
+    {
+        // calories count
+
+        // nutrients quantities :
+        // carbs
+        // proteins
+        // sugars
+        // fats
+        // salts
+    }
+}
