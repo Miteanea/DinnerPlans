e7037dade44737bce82162bbd0f40a4796ca4847
dmitri.caragheaur <dmitri.caragheaur@gmail.com>
2019-12-05 10:50:23 +0000

    EfCore added

---
+++ DinnerPlans/Services/Database/DinnerPlansContext.cs
@@ -0,0 +1,32 @@
+﻿using DinnerPlans.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinnerPlans.Services.Database
+{
+    class DinnerPlansContext : DbContext
+    {
+        public DinnerPlansContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = @"Server =.\SQLEXPRESS; Database = DinnerPlansDB; Trusted_Connection = True; ";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+        //entities
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+
+    }
+}
